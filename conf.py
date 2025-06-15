cc535b9db0d5b4d5d6d5b9b0c3d98f0ef831c56f
Xander Harris <xandertheharris@gmail.com>
2025-06-15 14:56:14 -0700

    Update pipfile
    Closes #7

--- conf.py
+++ conf.py
@@ -6,44 +6,43 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 """
+
 # pylint: disable=invalid-name,redefined-builtin
 
-author = 'Xander Harris'
-ansible_roles_path = ['.']
-ansible_tmp_dir = "/tmp/sphinx-ansible"
+author = "Xander Harris"
 autoyaml_root = "."
 autoyaml_depth = 10
 
-copyright = '2024, Xander Harris'
+copyright = "2024, Xander Harris"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 exclude_patterns = [
-    '_build',
-    'Thumbs.db',
-    '.DS_Store',
-    '.venv/*',
-    '.tmp/*',
-    '.pytest_cache/*',
-    '.venv/',
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    ".venv/*",
+    ".tmp/*",
+    ".pytest_cache/*",
+    ".venv/",
 ]
 
 extensions = [
-    'myst_parser',
-    'notfound.extension',
-    'sphinx_design',
-    'sphinx_favicon',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosectionlabel',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.duration',
-    'sphinx.ext.extlinks',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.inheritance_diagram',
-    'sphinxcontrib.autoyaml',
-    'sphinxcontrib.sphinx_ansible',
+    "myst_parser",
+    "notfound.extension",
+    "sphinx_design",
+    "sphinx_favicon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.duration",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.inheritance_diagram",
+    "sphinxcontrib.autoyaml",
+    "sphinxcontrib.sphinx_ansible",
 ]
 
 favicons = [
@@ -65,12 +64,12 @@ favicons = [
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_static_path = ['_static']
-html_theme = 'sphinx_nefertiti'
+html_static_path = ["_static"]
+html_theme = "sphinx_nefertiti"
 html_theme_options = {
-    'logo': 'img/ansible.png',
-    'repository_url': 'https://github.com/edwardtheharris/ansible-k8s-ca',
-    'repository_name': 'ansible k8s ca',
+    "logo": "img/ansible.png",
+    "repository_url": "https://github.com/edwardtheharris/ansible-k8s-ca",
+    "repository_name": "ansible k8s ca",
     "style": "blue",
 }
 myst_dmath_double_inline = True
@@ -92,10 +91,8 @@ myst_enable_extensions = [
     "tasklist",
 ]
 myst_title_to_header = True
-project = 'Ansible Certificate Authority'
-release = '0.0.1'
+project = "Ansible Certificate Authority"
+release = "0.0.1"
 show_authors = True
-source_suffix = {
-    '.md': 'markdown'
-}
-templates_path = ['_templates']
+source_suffix = {".md": "markdown"}
+templates_path = ["_templates"]
